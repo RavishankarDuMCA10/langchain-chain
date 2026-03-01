8047ed02ec3421d26f3d68f4885b00746ef4ac42
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2026-03-01 21:57:39 +0530

    Installed all required libraries using command: uv add langchain langchain-openai langchain-tavily tavily-python python-dotenv black isort Added Search function tool with static search result.

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 
+@tool
+def search(query: str) -> str:
+    """
+    Search the web for infromation
+    Args:
+        query: the query to search for
+    Returns:
+        The search results
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
     print("Hello from langchain-chain!")
+    result = agent.invoke(
+        {"messages": HumanMessage(content="What is the weather in Tokyo?")}
+    )
+    print(result)
 
 
 if __name__ == "__main__":
